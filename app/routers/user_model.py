f903267a168c2824328212231a694b2e356a9150
BM_YF <2922265540@qq.com>
2022-04-01 01:18:10 +0800

    🎉: 新增获取当前登录用户的接口

--- app/routers/user_model.py
+++ app/routers/user_model.py
@@ -1,12 +1,15 @@
 from sqlmodel import SQLModel, Field
 
 
-class UserSignUpReq(SQLModel):
+class UserBase(SQLModel):
+    nickname: str
+    avatar_url: str
+
+
+class UserSignUpReq(UserBase):
     phone_number: str = Field(max_length=11, description='仅支持中国大陆地区手机号')
     password: str = Field(min_length=8, max_length=30)
     code: str = Field(description='wx.login 接口返回的code')
-    nickname: str
-    avatar_url: str
 
 
 class LoginByOpenidReq(SQLModel):
